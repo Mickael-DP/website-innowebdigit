41d231fae70c12c3627472b66029a739a5d63cdd
Mickael DP <m.dallepasqualine@gmail.com>
2024-06-20 16:29:04 +0200

    Finish component FormInput

---
+++ src/app/components/FormInput/FormInput.tsx
@@ -0,0 +1,26 @@
+import { FormControl, FormHelperText, FormLabel, Input } from '@mui/joy';
+import { Box, TextField, Typography } from '@mui/material';
+
+interface FormInputProps {
+	label: string;
+	placeholder?: string;
+	required?: boolean;
+	type?: 'text' | 'email';
+}
+
+const FormInput = ({ label, required, type, placeholder }: FormInputProps) => (
+	<FormControl
+		required={required}
+		size='md'
+		color='neutral'>
+		<FormLabel sx={{color: 'white'}}>{label}</FormLabel>
+		<Input
+			placeholder={placeholder}
+			name={label}
+			type={type}
+			variant='outlined'
+		/>
+	</FormControl>
+);
+
+export default FormInput;
