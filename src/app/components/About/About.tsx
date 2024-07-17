62bd3153f6850b1cf369a03dbe6dfe55c2608196
Mickael DP <m.dallepasqualine@gmail.com>
2024-07-17 12:32:55 +0200

    recentre CTA hero

--- src/app/components/About/About.tsx
+++ src/app/components/About/About.tsx
@@ -4,26 +4,28 @@ import FileDownloadIcon from '@mui/icons-material/FileDownload';
 
 const About = () => {
 	return (
-		<Box sx={{
-            display: 'flex',
-			flexDirection: { xs: 'column', md: 'row' },
-            justifyContent: { xs: 'center', md: 'space-between'},
-            paddingX: { md: 25},
-            mt: 8,
-            width: '100%',
-        }}>
-			<Box sx={{
-                mr: { xs: 0, md: 25},
-				mx: { xs: 5, md: 0},
-                display: 'flex',
-                flexDirection: 'column',
-                justifyContent: 'space-between',
-            }}>
+		<Box
+			sx={{
+				display: 'flex',
+				flexDirection: { xs: 'column', md: 'row' },
+				justifyContent: { xs: 'center', md: 'space-between' },
+				paddingX: { md: 25 },
+				mt: 8,
+				width: '100%',
+			}}>
+			<Box
+				sx={{
+					mr: { xs: 0, md: 25 },
+					mx: { xs: 5, md: 0 },
+					display: 'flex',
+					flexDirection: 'column',
+					justifyContent: 'space-between',
+				}}>
 				<Typography
 					variant='h3'
 					sx={{
 						fontFamily: 'Fira Sans, sans-serif',
-							fontWeight: '800',
+						fontWeight: '800',
 					}}
 					gutterBottom
 					color='white'>
@@ -33,7 +35,7 @@ const About = () => {
 					variant='h6'
 					sx={{
 						fontFamily: 'Fira Sans, sans-serif',
-							fontWeight: '400',
+						fontWeight: '400',
 					}}
 					gutterBottom
 					color='#C7C7C7'>
@@ -47,12 +49,12 @@ const About = () => {
 					velit eu eget ac tortor. Ac sit ultricies hendrerit pellentesque arcu
 					fames sit lectus. Scelerisque id bibendum velit cras.
 				</Typography>
-                <Typography
+				<Typography
 					variant='h6'
 					gutterBottom
 					sx={{
 						fontFamily: 'Fira Sans, sans-serif',
-							fontWeight: '400',
+						fontWeight: '400',
 					}}
 					color='#C7C7C7'>
 					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Blandit id
@@ -65,32 +67,35 @@ const About = () => {
 					velit eu eget ac tortor. Ac sit ultricies hendrerit pellentesque arcu
 					fames sit lectus. Scelerisque id bibendum velit cras.
 				</Typography>
-				<Box sx={{display:{xs:'flex'}, mx: {xs:1}}}>
-					<Button sx={{
-                        backgroundColor: '#59E2E8',
-                        borderRadius: 90,
-                        padding: { xs: 1, sm: 2, md: 2},
-                        color: 'black',
-						fontFamily: 'Fira Sans, sans-serif',
-                        fontWeight: 'bold',
-                        mr: 2,
-						fontSize: { xs: '0.8rem', sm: '1rem', md: '1rem' },
-
-                    }} endIcon={<FileDownloadIcon/>}>Télécharger mon CV -</Button>
-					<Button  sx={{
-                        backgroundColor: '#181818',
-                        borderRadius: 180,
-                        padding:2.5,
-                        color: '#59E2E8',
-                        fontWeight: 'bold',
-						fontSize: { xs: '0.8rem', sm: '1rem', md: '1rem' },
-
-                    }} >
+				<Box sx={{ display: { xs: 'flex' }, mx: { xs: 1 } }}>
+					<Button
+						sx={{
+							backgroundColor: '#59E2E8',
+							borderRadius: 90,
+							padding: { xs: 1, sm: 2, md: 2 },
+							color: 'black',
+							fontFamily: 'Fira Sans, sans-serif',
+							fontWeight: 'bold',
+							mr: 2,
+							fontSize: { xs: '0.8rem', sm: '1rem', md: '1rem' },
+						}}
+						endIcon={<FileDownloadIcon />}>
+						Télécharger mon CV -
+					</Button>
+					<Button
+						sx={{
+							backgroundColor: '#181818',
+							borderRadius: 180,
+							padding: 2.5,
+							color: '#59E2E8',
+							fontWeight: 'bold',
+							fontSize: { xs: '0.8rem', sm: '1rem', md: '1rem' },
+						}}>
 						<LinkedIn />
 					</Button>
 				</Box>
 			</Box>
-			<Box sx={{mt: {xs: 5}}}>
+			<Box sx={{ mt: { xs: 5 } }}>
 				<img
 					src='https://via.placeholder.com/450x560'
 					alt='placeholder'
--- src/app/components/Hero/Hero.tsx
+++ src/app/components/Hero/Hero.tsx
@@ -52,10 +52,12 @@ const Hero: React.FC = () => {
 						fontFamily='Fire Sans, sans-serif'
 						fontWeight='300'
 						sx={{
-							fontSize: { xs: '2rem', sm: '3rem', md: '3rem' }, 
+							fontSize: { xs: '2rem', sm: '3rem', md: '3rem' },
 						}}>
-					BIENVENUE <br/> CHEZ <br /> 
-                        <span style={{ color: '#59E2E8', fontWeight:'800' }}>INNOWEB DIGITAL</span>
+						BIENVENUE <br /> CHEZ <br />
+						<span style={{ color: '#59E2E8', fontWeight: '800' }}>
+							INNOWEB DIGITAL
+						</span>
 					</Typography>
 					<Typography
 						variant='h5'
@@ -64,7 +66,7 @@ const Hero: React.FC = () => {
 						fontWeight='100'
 						marginTop='30px'
 						sx={{
-							fontSize: { xs: '1rem', sm: '1.5rem', md: '2rem' }, 
+							fontSize: { xs: '1rem', sm: '1.5rem', md: '2rem' },
 						}}>
 						Votre Développeur web freelance
 					</Typography>
@@ -75,7 +77,7 @@ const Hero: React.FC = () => {
 						fontWeight='100'
 						marginBottom='30px'
 						sx={{
-							fontSize: { xs: '1rem', sm: '1.5rem', md: '2rem' }, 
+							fontSize: { xs: '1rem', sm: '1.5rem', md: '2rem' },
 						}}>
 						Prêt a améliorer ton expérence digital en fonction de tes besoins
 					</Typography>
@@ -85,25 +87,30 @@ const Hero: React.FC = () => {
 							'& > *': {
 								margin: 1,
 							},
-							display: {xs: 'flex', sm: 'flex' },
-							flexDirection: {xs: 'column', sm: 'row' },
-							justifyContent: {xs: 'center', sm: 'center' },
-							alignItems: {xs: 'center', sm: 'center' },
-							marginX: {xs: '30px' },
+							display: { xs: 'flex', sm: 'flex' },
+							flexDirection: { xs: 'column', sm: 'row' },
+							alignItems: { xs: 'center', sm: 'center' },
+							marginX: { xs: '30px' },
 						}}>
 						<Button
 							variant='solid'
-                            size='lg'
-							sx={{ backgroundColor: '#181818', mr: 5, fontSize: { xs: '0.8rem', sm: '1rem', md: '1rem' },  }}
-							>
-							Me découvrir <ArrowDownwardIcon sx={{ml: 1}} />
+							size='lg'
+							sx={{
+								backgroundColor: '#181818',
+								mr: 5,
+								fontSize: { xs: '0.8rem', sm: '1rem', md: '1rem' },
+							}}>
+							Me découvrir <ArrowDownwardIcon sx={{ ml: 1 }} />
 						</Button>
 						<Button
 							variant='solid'
-                            size='lg'
-							sx={{ backgroundColor: '#59E2E8', color:'#000000', fontSize: { xs: '0.8rem', sm: '1rem', md: '1rem' }, }}
-							>
-							Demander un devis <SendIcon sx={{ml: 1}} />
+							size='lg'
+							sx={{
+								backgroundColor: '#59E2E8',
+								color: '#000000',
+								fontSize: { xs: '0.8rem', sm: '1rem', md: '1rem' },
+							}}>
+							Demander un devis <SendIcon sx={{ ml: 1 }} />
 						</Button>
 					</Box>
 				</Box>
